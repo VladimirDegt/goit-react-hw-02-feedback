87679e8fde621776af700761b3e65bd33d8cc9a3
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-05-22 18:02:20 +0300

    update prop-type

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -32,4 +32,7 @@ Section.propTypes = {
   title: PropTypes.string.isRequired,
   numberSection: PropTypes.string.isRequired,
   onLeaveFeedBack: PropTypes.func,
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
 };
